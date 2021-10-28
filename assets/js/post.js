9a4257c9d214afeefd95ace67b1ba1fd2f66874d
Augus <bramlye@outlook.com>
2021-10-28 10:16:15 +0800

    update js 目录缩进及全局图片设置

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,25 +1,26 @@
-var postContent = document.querySelector('#post-content');
-var imgs = postContent.querySelectorAll('img');
-var len = imgs.length;
+const postContent = document.querySelector('#post-content');
+const allImg = postContent.querySelectorAll('img');
+const len = allImg.length;
 
 // 格式化显示所有文章图片
 for (let i = 0; i < len; i++) {
-  imgs[i].classList.add('materialboxed');
-  imgs[i].classList.add('z-depth-4');
+    // allImg[i].classList.add('materialboxed');//放大图片会造成图片往左侧栏内移入
+    allImg[i].classList.add('z-depth-4');
+    allImg[i].classList.add('responsive-img')
 }
 
 // 初始化侧栏目录插件
 document.addEventListener('DOMContentLoaded', function () {
-  var elemCategory = document.querySelector('#category');
-  M.Sidenav.init(elemCategory, {
-    'edge': 'right'
-  });
+    const elemCategory = document.querySelector('#category');
+    M.Sidenav.init(elemCategory, {
+        'edge': 'right'
+    });
 });
 
 // 选中文章内容并复制
 document.addEventListener('keyup', e => {
-  if (e.key === 'A' && e.ctrlKey && e.shiftKey) {
-    getSelection().selectAllChildren(postContent);
-    document.execCommand('copy');
-  }
+    if (e.key === 'A' && e.ctrlKey && e.shiftKey) {
+        getSelection().selectAllChildren(postContent);
+        document.execCommand('copy');
+    }
 })
