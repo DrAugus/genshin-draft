110b174fe0e40ae59351ae2ac01046251625c406
Augus <bramlye@outlook.com>
2021-11-12 10:12:40 +0800

    update genshin-impact timeline

--- assets/js/genshin-event-timeline.js
+++ assets/js/genshin-event-timeline.js
@@ -1091,49 +1091,6 @@ setInterval("time_str.innerHTML = new Date().toString() + ' 星期' + '日一二
 setInterval("deadline.innerHTML = wishDeadline();", 1000);
 
 
-for (let i = 0; i < length; ++i) {
-    // document.getElementById('imgCharacter' + i).src = searchGenshinResUrl + "/assets/res/genshin-impact/events/" + eventsCharacters[i].image;
-    // document.getElementById('imgWeapon' + i).src = searchGenshinResUrl + "/assets/res/genshin-impact/events/" + eventsWeapons[i].image;
-
-    // document.getElementById('aCharacter' + i).href = aCharacters[i];
-    // document.getElementById('aWeapon' + i).href = aWeapons[i];
-
-    // document.getElementById('nameCharacter' + i).innerHTML = eventsCharacters[i].name[1] + " " + eventsCharacters[i].shortname[1] + " " + eventsCharacters[i].name[0];
-    // document.getElementById('nameWeapon' + i).innerHTML = eventsWeapons[i].name[1] + eventsWeapons[i].name[0];
-
-    document.getElementById('timeStartCharacter' + i).innerHTML = eventsCharacters[i].start;
-    document.getElementById('timeEndCharacter' + i).innerHTML = eventsCharacters[i].end;
-    // document.getElementById('timeStartWeapon' + i).innerHTML = timeStartWeapon[i];
-    // document.getElementById('timeEndWeapon' + i).innerHTML = timeEndWeapon[i];
-
-    //改变颜色
-    // document.getElementById('nameCharacter' + i).style.color = eventsCharacters[i].color;
-    // document.getElementById('nameWeapon' + i).style.color = colorWeapon;
-
-
-    //动态设置各个角色的css
-    let eventItemClass = document.getElementsByClassName('event-item-' + i);
-    for (let eItem of eventItemClass) {
-        eItem.style.backgroundImage = "url('/assets/res/genshin-impact/events/" + eventsCharacters[i].image + "')";
-        eItem.style.backgroundPosition = eventsCharacters[i].pos;
-        eItem.style.backgroundSize = eventsCharacters[i].zoom ? eventsCharacters[i].zoom : '200%';
-    }
-
-    let eventNameColor = eventsCharacters[i].color;
-    let eventNameClass = document.getElementsByClassName('event-name-' + i);
-    for (let eName of eventNameClass) {
-        eName.style.textShadow = eventNameColor + ' -1px -1px 4px, ' + eventNameColor + ' 1px -1px 4px, ' +
-            eventNameColor + ' -1px 1px 4px, ' + eventNameColor + ' 1px 1px 4px, ' + eventNameColor + ' 0 0 10px';
-    }
-
-    //left-t
-    let leftClass = document.getElementsByClassName('left-t' + i);
-    for (let lClass of leftClass) {
-        lClass.style.left = (150 * i) + 'px';
-    }
-}
-
-
 let dayWidth = 50;
 const eventHeight = 36;
 const eventMargin = 20;
@@ -1153,7 +1110,7 @@ let today = dayjs();
 function convertToDate(e, i) {
     let start = dayjs(e.start, 'YYYY-MM-DD HH:mm:ss').subtract(0, 'minute');
     const end = dayjs(e.end, 'YYYY-MM-DD HH:mm:ss').subtract(0, 'minute');
-    const duration = end.diff(start, 'seconds', true);
+    const duration = end.diff(start, 'day', true);
 
     if (lastEventTime < end) lastEventTime = end;
 
@@ -1297,9 +1254,9 @@ for (let i = 0; i < monthList.length; ++i) {
 
 let todayOffset = Math.abs(firstDay.diff(today, 'day', true));
 let leftToday = document.getElementsByClassName('timeline-line-pos');
-for(let l of leftToday){
+for (let l of leftToday) {
     //timeline-index的width为30
-    l.style.left = todayOffset * dayWidth +30 + 'px';
+    l.style.left = todayOffset * dayWidth + 30 + 'px';
 }
 
 
@@ -1315,4 +1272,58 @@ const setTodayTime = () => {
     document.getElementById('today-time').innerHTML = d.format('HH:mm:ss')
 }
 setInterval("setTodayTime()", 1000);
+
+
+for (let i = 0; i < length; ++i) {
+    // document.getElementById('imgCharacter' + i).src = searchGenshinResUrl + "/assets/res/genshin-impact/events/" + eventsCharacters[i].image;
+    // document.getElementById('imgWeapon' + i).src = searchGenshinResUrl + "/assets/res/genshin-impact/events/" + eventsWeapons[i].image;
+
+    // document.getElementById('aCharacter' + i).href = aCharacters[i];
+    // document.getElementById('aWeapon' + i).href = aWeapons[i];
+
+    // document.getElementById('nameCharacter' + i).innerHTML = eventsCharacters[i].name[1] + " " + eventsCharacters[i].shortname[1] + " " + eventsCharacters[i].name[0];
+    // document.getElementById('nameWeapon' + i).innerHTML = eventsWeapons[i].name[1] + eventsWeapons[i].name[0];
+
+    document.getElementById('timeStartCharacter' + i).innerHTML = eventsCharacters[i].start;
+    document.getElementById('timeEndCharacter' + i).innerHTML = eventsCharacters[i].end;
+    // document.getElementById('timeStartWeapon' + i).innerHTML = timeStartWeapon[i];
+    // document.getElementById('timeEndWeapon' + i).innerHTML = timeEndWeapon[i];
+
+    //改变颜色
+    // document.getElementById('nameCharacter' + i).style.color = eventsCharacters[i].color;
+    // document.getElementById('nameWeapon' + i).style.color = colorWeapon;
+
+
+    //动态设置各个角色的css
+    let eventItemClass = document.getElementsByClassName('event-item-' + i);
+
+    let e = events[4]
+    let start = firstDay
+    const end = dayjs(e[i].start, 'YYYY-MM-DD HH:mm:ss').subtract(0, 'minute');
+    const duration = end.diff(start, 'day', true);
+    console.log(i, duration)
+
+    for (let eItem of eventItemClass) {
+        eItem.style.backgroundImage = "url('/assets/res/genshin-impact/events/" + eventsCharacters[i].image + "')";
+        eItem.style.backgroundPosition = eventsCharacters[i].pos;
+        eItem.style.backgroundSize = eventsCharacters[i].zoom ? eventsCharacters[i].zoom : '200%';
+        eItem.style.width = e[i].duration * dayWidth + 'px'
+        console.log(eItem.style.width)
+        eItem.style.left = duration * dayWidth + 30 + 'px'
+        console.log(eItem.style.left)
+    }
+
+    let eventNameColor = eventsCharacters[i].color;
+    let eventNameClass = document.getElementsByClassName('event-name-' + i);
+    for (let eName of eventNameClass) {
+        eName.style.textShadow = eventNameColor + ' -1px -1px 4px, ' + eventNameColor + ' 1px -1px 4px, ' +
+            eventNameColor + ' -1px 1px 4px, ' + eventNameColor + ' 1px 1px 4px, ' + eventNameColor + ' 0 0 10px';
+    }
+
+    //left-t
+    let leftClass = document.getElementsByClassName('left-t' + i);
+    for (let lClass of leftClass) {
+        lClass.style.left = (350 * i) + 'px';
+    }
+}
 
