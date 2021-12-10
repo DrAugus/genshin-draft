e02d4e37e592f6216ab99639dad7a33bea6e4bba
Augus <bramlye@outlook.com>
2021-12-10 11:13:21 +0800

    update time.js 时间方法提取为公共方法

--- assets/js/genshin-event-timeline.js
+++ assets/js/genshin-event-timeline.js
@@ -1181,38 +1181,6 @@ for (let i = 0; i < voiceHuTao.length; ++i) {
 }
 
 
-//秒转换
-const secondsFormat = (s) => {
-    const day = Math.floor(s / (24 * 3600)); // Math.floor()向下取整
-    const hour = Math.floor((s - day * 24 * 3600) / 3600);
-    const minute = Math.floor((s - day * 24 * 3600 - hour * 3600) / 60);
-    const second = s - day * 24 * 3600 - hour * 3600 - minute * 60;
-    return day + "天" + hour + "时" + minute + "分" + second + "秒";
-}
-
-//返回始末时间之间的所有日期
-const getDuration = (type, start, end) => {
-    const $array = [];
-    const current = new Date(start);
-    end = new Date(end);
-    while (current <= end) {
-        $array.push(new Date(current));
-        if (type == 'hour') {//小时
-            current.setHours(current.getHours() + 1);
-        } else if (type == 'day') {//天
-            current.setDate(current.getDate() + 1);
-        } else if (type == 'week') {//周
-            current.setDate(current.getDate() + 7);
-        } else if (type == 'month') {//月
-            current.setMonth(current.getMonth() + 1);
-        } else {//默认天
-            current.setDate(current.getDate() + 1);
-        }
-    }
-    return $array;
-}
-
-
 // main func
 
 let dayWidth = 35;
@@ -1631,8 +1599,6 @@ const futureWishInfo = () => {
 }
 futureWishInfo();
 
-
-const Deadline = (start, end) => secondsFormat(Math.floor(end.diff(start) / 1000));
 
 //祈愿倒计时
 const wishDeadline = () => Deadline(dayjs(), dayjs(wishCharacters[currentWishObj.wishIndex[0]].end))
