dcb43ce33fbdab50c77d9d1fe785a0f910609bd6
Augus <bramlye@outlook.com>
2021-12-17 14:09:35 +0800

    update remove多余代码

--- assets/js/post.js
+++ assets/js/post.js
@@ -8,19 +8,3 @@ for (let i = 0; i < len; i++) {
     allImg[i].classList.add('z-depth-4');
     allImg[i].classList.add('responsive-img')
 }
-
-// 初始化侧栏目录插件
-document.addEventListener('DOMContentLoaded', function () {
-    const elemCategory = document.querySelector('#category');
-    M.Sidenav.init(elemCategory, {
-        'edge': 'right'
-    });
-});
-
-// 选中文章内容并复制
-document.addEventListener('keyup', e => {
-    if (e.key === 'A' && e.ctrlKey && e.shiftKey) {
-        getSelection().selectAllChildren(postContent);
-        document.execCommand('copy');
-    }
-})
