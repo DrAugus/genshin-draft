f5d4903c3464fa3724e81677e1db22c2255082e3
Augus <bramlye@outlook.com>
2021-12-20 18:25:12 +0800

    update time.js 配色

--- assets/js/time.js
+++ assets/js/time.js
@@ -6,11 +6,9 @@ const secondsFormat = (s) => {
     const second = s - day * 24 * 3600 - hour * 3600 - minute * 60;
     if (day < 0 || hour < 0 || minute < 0 || second < 0) return -1;
     return day + "天" + hour + "时" + minute + "分" + second + "秒";
-}
-
+};
 
 const Deadline = (start, end) => secondsFormat(Math.floor(end.diff(start) / 1000));
-
 
 //返回始末时间之间的所有日期
 const getDuration = (type, start, end) => {
@@ -19,31 +17,33 @@ const getDuration = (type, start, end) => {
     end = new Date(end);
     while (current <= end) {
         $array.push(new Date(current));
-        if (type == 'hour') {//小时
+        if (type == "hour") {//小时
             current.setHours(current.getHours() + 1);
-        } else if (type == 'day') {//天
+        } else if (type == "day") {//天
             current.setDate(current.getDate() + 1);
-        } else if (type == 'week') {//周
+        } else if (type == "week") {//周
             current.setDate(current.getDate() + 7);
-        } else if (type == 'month') {//月
+        } else if (type == "month") {//月
             current.setMonth(current.getMonth() + 1);
         } else {//默认天
             current.setDate(current.getDate() + 1);
         }
     }
     return $array;
-}
+};
 
-const ddlCSS = (id,d) => {
+const ddlCSS = (id, d) => {
+    $(id).addClass("black-text");
+    let color = ["#eda2b6", "#eb8f90", "#abbf4e", "#abbf4e", "#abbf4e", "#abbf4e"];
     if (d < 3) {
-        $(id).addClass('red black-text')
+        $(id).css("background-color", color[0]);
     } else if (d < 7) {
-        $(id).addClass('orange black-text')
+        $(id).css("background-color", color[1]);
     } else if (d < 10) {
-        $(id).addClass('teal black-text')
+        $(id).css("background-color", color[2]);
     } else if (d < 15) {
-        $(id).addClass('blue black-text')
+        $(id).css("background-color", color[3]);
     } else {
-        $(id).addClass('green black-text')
+        $(id).css("background-color", color[4]);
     }
-}
+};
