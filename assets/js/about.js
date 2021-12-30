fd10effb7a8bb9e1598c6eee943bde80588fcc19
Augus <bramlye@outlook.com>
2021-12-30 13:22:17 +0800

    update about.html

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,8 +1,21 @@
 $(document).ready(function () {
-    $(".user-intro h4").removeClass("hidden");
+    $(".user-intro h4").removeClass("hide");
     $("#js-rotating").Morphext({
         animation: "flip",
         separator: ",",
         speed: 2000
     });
 });
+
+
+function subtitleType() {
+    const typed = new Typed("#subtitle", {
+        strings: "占位符,There is a sweet smell of roses in the air.,kiss you on the cheek,love at fist sight".split(","),
+        startDelay: 300,
+        typeSpeed: 150,
+        loop: true,
+        backSpeed: 50
+    });
+}
+
+subtitleType();
