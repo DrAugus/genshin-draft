616d99aa3ed28e539ebc42e316dc8efcb1af3795
Augus <bramlye@outlook.com>
2022-01-03 14:46:06 +0800

    update genshin.html全部角色筛选

--- assets/js/genshin.js
+++ assets/js/genshin.js
@@ -11,6 +11,28 @@ const elementColor = {
 const colorWeapon = "#FFAA4B";
 
 
+const eleArr = ["dendro", "geo", "electro", "hydro", "pyro", "anemo", "cryo",];
+const showEle = (show) => {
+    for (let e of eleArr) {
+        if (e === show) $(".show-" + e).removeClass("hide");
+        else $(".show-" + e).addClass("hide");
+    }
+};
+const showAll = () => {
+    for (let e of eleArr) {
+        console.log(e);
+        $(".show-" + e).removeClass("hide");
+    }
+};
+//默认 展示所有
+showAll();
+for (let e of eleArr) {
+    $("#" + e).click(() => {
+        showEle(e);
+    });
+}
+
+
 //胡桃 语音
 const voiceHuTao = [
     {
