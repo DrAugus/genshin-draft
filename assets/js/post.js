743d1ef43144f62e386735060dd2bd52eeac7a92
Augus <bramlye@outlook.com>
2022-01-04 15:22:58 +0800

    fix post.js不存在对应id时的异常

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,10 +1,14 @@
-const postContent = document.querySelector("#all-contents-info");
-const allImg = postContent.querySelectorAll("img");
-const len = allImg.length;
-
-// 格式化显示所有文章图片
-for (let i = 0; i < len; i++) {
-    allImg[i].classList.add("materialboxed");
-    allImg[i].classList.add("z-depth-4");
-    allImg[i].classList.add("responsive-img");
-}
+const formatAllImg = () => {
+    const postContent = document.querySelector("#all-contents-info");
+    if (!postContent) return;
+    const allImg = postContent.querySelectorAll("img");
+    if (!allImg) return;
+    const len = allImg.length;
+    // 格式化显示所有文章图片
+    for (let i = 0; i < len; i++) {
+        allImg[i].classList.add("materialboxed");
+        allImg[i].classList.add("z-depth-4");
+        allImg[i].classList.add("responsive-img");
+    }
+};
+formatAllImg();
