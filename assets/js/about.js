ea66814df138785e2b9255a2640812acae7c99f2
Augus <bramlye@outlook.com>
2022-01-06 23:55:45 +0800

    update words-en.yaml

--- assets/js/about.js
+++ assets/js/about.js
@@ -10,7 +10,11 @@ $(document).ready(function () {
 
 function subtitleType() {
     const typed = new Typed("#subtitle", {
-        strings: "占位符,There is a sweet smell of roses in the air.,kiss you on the cheek,love at fist sight".split(","),
+        strings: "占位符," +
+            "There is a sweet smell of roses in the air.," +
+            "kiss you on the cheek,love at fist sight," +
+            "In that minute, Troy had kissed her on the lips," +
+            "".split(","),
         startDelay: 300,
         typeSpeed: 150,
         loop: true,
