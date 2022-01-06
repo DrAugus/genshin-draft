7ffdc04ccabae49b84177a53382e351d89ecae8b
Augus <bramlye@outlook.com>
2022-01-06 14:14:27 +0800

    update post.js代码实现优化

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,14 +1,4 @@
 const formatAllImg = () => {
-    const postContent = document.querySelector("#all-contents-info");
-    if (!postContent) return;
-    const allImg = postContent.querySelectorAll("img");
-    if (!allImg) return;
-    const len = allImg.length;
-    // 格式化显示所有文章图片
-    for (let i = 0; i < len; i++) {
-        allImg[i].classList.add("materialboxed");
-        allImg[i].classList.add("z-depth-4");
-        allImg[i].classList.add("responsive-img");
-    }
+    $(".img").addClass("materialboxed z-depth-4 responsive-img");
 };
 formatAllImg();
