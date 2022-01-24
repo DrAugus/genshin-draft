06d95361090fa2325e82116e60c55a5abfe2b6ca
Augus <bramlye@outlook.com>
2022-01-24 14:37:25 +0800

    update 移除外卖界面

--- assets/js/augus.js
+++ assets/js/augus.js
@@ -10,3 +10,19 @@ const obj2arr = (obj) => {
     }
     return arr;
 };
+
+
+const findKey = (value, compare = (a, b) => a === b) => Object.keys(data).find(k => compare(data[k], value));
+
+
+let bestFood = [];
+let length = bestFood.length;
+
+//key 为 name
+let repeatName = new Map();
+bestFood.forEach(v => !repeatName.has(v.name) ? repeatName.set(v.name, v) : repeatName.set(v.name, [repeatName.get(v.name), v]));
+//移除非重复的
+repeatName.forEach((value, key) => {
+    if (!Array.isArray(value))
+        repeatName.delete(key);
+});
--- page/life/food.md
+++
@@ -1,7 +0,0 @@
----
-permalink: /food
-layout: page
-title: "人生难题——吃什么"
-js: ["food-list.js"]
----
-{% include life/food.html %}
