b8836e7e03475f6cdd6b465bcaf04c7c45bb2681
Augus <bramlye@outlook.com>
2022-02-08 11:15:52 +0800

    update genshin level.html
    实现更多细节

--- assets/js/genshin/level.js
+++ assets/js/genshin/level.js
@@ -13,7 +13,61 @@ const EXP = [
     232350, 258950, 285750, 312825, 340125
 ];
 
-const getEXP = (lv1, lv2) => {
+//8min恢复一个原脆树脂
+const RENEW = 8;
+//一天180个树脂
+const TOTAL_DAY = 24 * 60 / RENEW;
+//氪佬 每天最多买6次 一次60树脂
+const TU_HAO = [0, 50, 100, 100, 150, 200, 200];
+//经验树脂比 为4:1
+const resin2exp = resin => resin * 4;
+
+const getDaySpend = (exp, money) => {
+    let cost = 0;
+    let resinPlus = 0;
+    if (money) {
+        for (let i = 0; i <= money; ++i) {
+            cost += TU_HAO[i];
+        }
+        resinPlus = 60 * money;
+    }
+
+    let resinDayTotal = TOTAL_DAY + resinPlus;
+    let dayEXPTotal = resin2exp(resinDayTotal);
+    let daySpend = Math.ceil(exp / dayEXPTotal);
+    console.info("exp", exp, "resin", resinDayTotal, "daySpend", daySpend);
+    return {
+        daySpend: daySpend,
+        cost: cost
+    };
+};
+
+const showMore = (obj) => {
+    let prefixInfo = "";
+    let day = obj.daySpend;
+    let dayInfo = "";
+
+    if (obj.cost === 0) prefixInfo = "白嫖的神";
+    else if (obj.cost === 800) prefixInfo = "氪佬悠着点啊";
+    else prefixInfo = "氪佬大气";
+
+    if (day < 7) {
+        dayInfo = "仅剩" + day + "天，冲冲冲！";
+    } else if (day < 30) {
+        dayInfo = "仅剩" + day + "天，很快了！冲冲冲！！";
+    } else if (day < 180) {
+        dayInfo = "还有" + day + "天，加油";
+    } else if (day < 365) {
+        dayInfo = "还有" + day + "天，慢慢来";
+    } else {
+        dayInfo = "遥遥无期，还得" + day + "天啊";
+    }
+
+    $("#prefix_info").text(prefixInfo);
+    $("#need_day").text(dayInfo);
+};
+
+const getEXP = (lv1, lv2, haveGot, money) => {
     let show = "非法输入，请正确输入";
 
     if (lv1 >= lv2 || lv1 > 59 || lv2 > 60) {
@@ -23,6 +77,15 @@ const getEXP = (lv1, lv2) => {
         for (let i = lv1; i < lv2; ++i) {
             exp += EXP[i];
         }
+
+        console.warn("exp", exp);
+
+        haveGot = parseInt(haveGot);
+        if (haveGot > exp) return show;
+
+        exp -= haveGot;
+        console.warn("haveGot", haveGot);
+        showMore(getDaySpend(exp, money));
         show = "" + exp;
     }
 
@@ -33,5 +96,7 @@ const getEXP = (lv1, lv2) => {
 const showEXP = () => {
     let lv1 = $("#lv1").val();
     let lv2 = $("#lv2").val();
-    $("#need-exp").text(getEXP(lv1, lv2));
+    let money = $("#tu_hao").val();
+    let haveGot = $("#have_got").val();
+    $("#need-exp").text(getEXP(lv1, lv2, haveGot, money));
 };
